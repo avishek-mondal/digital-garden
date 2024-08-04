e29a7150216c119ff7e2213ab7cb299be4ed7708
avishek-mondal <mondal.avishekri@gmail.com>
2024-08-04 19:48:49 +0100

    new post

--- site/javascripts/mathjax.js
+++ site/javascripts/mathjax.js
@@ -1,26 +1,22 @@
 window.MathJax = {
-  loader: {load: ['[tex]/ams', '[tex]/newcommand', '[tex]/extpfeil']},
+  loader: {load: ['[tex]/ams', '[tex]/newcommand', '[tex]/extpfeil', '[tex]/color', '[tex]/physics']},
   tex: {
-    packages: {'[+]': ['ams', 'newcommand', 'extpfeil']},
+    packages: {'[+]': ['ams', 'newcommand', 'extpfeil', 'color', 'physics']},
     inlineMath: [["\\(", "\\)"]],
-      displayMath: [["\\[", "\\]"]],
-      processEscapes: true,
-      processEnvironments: true,
-      macros: {
-        // smash: ["\\mathllap{#1}", 1]
-        mathrlap: ["\\mathchoice{\\rlap{\\displaystyle{#1}}}{\\rlap{\\textstyle{#1}}}{\\rlap{\\scriptstyle{#1}}}{\\rlap{\\scriptscriptstyle{#1}}}", 1],
-        mathllap: ["\\mathchoice{\\llap{\\displaystyle{#1}}}{\\llap{\\textstyle{#1}}}{\\llap{\\scriptstyle{#1}}}{\\llap{\\scriptscriptstyle{#1}}}", 1],
-        mathclap: ["\\mathchoice{\\clap{\\displaystyle{#1}}}{\\clap{\\textstyle{#1}}}{\\clap{\\scriptstyle{#1}}}{\\clap{\\scriptscriptstyle{#1}}}", 1],
-        mysmash: ["\\mathrlap{\\phantom{#1}}#1", 1]
-      }
-    },
-    options: {
-      ignoreHtmlClass: ".*|",
-      processHtmlClass: "arithmatex"
+    displayMath: [["\\[", "\\]"]],
+    processEscapes: true,
+    processEnvironments: true,
+    macros: {
+      whitetextemdash: ["\\textcolor{white}{\\raise0.5ex{\\rule{1cm}{0.4pt}}}", 0]
+
     }
-  };
-  
-  document$.subscribe(() => {
-    MathJax.typesetPromise()
-  })
-  
+  },
+  options: {
+    ignoreHtmlClass: ".*|",
+    processHtmlClass: "arithmatex"
+  }
+};
+
+document$.subscribe(() => {
+  MathJax.typesetPromise()
+})
